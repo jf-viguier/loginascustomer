8a81e15fd0860ae229e9d04bb60b13b126adc4a3
Dindault Lucas <dindault.lucas@gmail.com>
2025-03-12 10:20:21 +0100

    Add translations for the back office

---
+++ translations/fr.php
@@ -0,0 +1,6 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{loginascustomer}prestashop>loginascustomer_9698fd0abac915053f837516d2c73127'] = 'Se connecter en tant que client';
+$_MODULE['<{loginascustomer}prestashop>loginascustomer_9dc571172f8a647d974cc00f28d529fb'] = 'Se connecter en tant que ';
